68485bad5c99dd9c5ba4adc7f90f0ab05dc9b46a
Quinn McDonnell <quinn.mcdonnell@mymail.champlain.edu>
2024-02-06 16:14:27 -0500

    Character Is implemented but needs work to actually function

---
+++ Super RNG Bros/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingpower = 16f;
+    private bool isFacingRight = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);   
+
+        if(!isFacingRight && horizontal > 0f)
+        {
+            flip();
+        }
+        else if(isFacingRight && horizontal < 0f)
+        {
+            flip();
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        
+        if(context.performed && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingpower);
+        }
+        
+        if(context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+}
