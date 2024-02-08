bb5d70de54e24536ed7dad112b2c2e6d94dca031
Quinn McDonnell <quinn.mcdonnell@mymail.champlain.edu>
2024-02-08 16:40:35 -0500

    Timer and Game Manager have been implemented

---
+++ Super RNG Bros/Assets/scripts/timer.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public float TimeLeft;
+    public Text timerTxt;
+
+    private float originalTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalTime = TimeLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+            if(TimeLeft >= -1)
+            {
+                TimeLeft -= Time.deltaTime;
+                updateTimer(TimeLeft);
+            }
+            else
+            {
+            TimeLeft = originalTime;
+            }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = (int)Math.Floor(currentTime / 60);
+        float seconds = (int)Math.Floor(currentTime % 60);
+
+
+
+        timerTxt.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+}
