1a82dfdbfa3e9f6e3c14269b58e207ad2e2cf8f9
Quinn McDonnell <quinn.mcdonnell@mymail.champlain.edu>
2024-04-03 19:52:35 -0400

    Figure out how to make continuous 1

---
+++ GPR-300-Assignment4/Assets/FollowPath.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    public curves[] curves;
+
+    private int i = 0;
+    private float u = 0.0f;
+    private int direction = 1;
+
+    private Vector3 GetCurve(curves c)
+    {
+        var p0 = c.point0.position;
+        var p1 = c.point1.position;
+        var p2 = c.point2.position;
+        var p3 = c.point3.position;
+
+        return c.cubicBezierCurve(u - i, p0, p1, p2, p3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dt = Time.deltaTime * 0.25f;
+        i = (int)Math.Floor(u);
+
+        //Debug.Log("I: " + i);
+        //Debug.Log("U: " + u);
+
+        if (u > curves.Length)
+        {
+            u = 0.0f;
+            i = 0;
+        }
+        
+
+        var current_curve = curves[i];
+        transform.position = GetCurve(current_curve);
+        
+        // always last?
+        u += dt * direction;
+        Debug.Log(u);
+    }
+
+}
--- GPR-300-Assignment4/Assets/SplineManager.cs
+++ GPR-300-Assignment4/Assets/SplineManager.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class SplineManager : MonoBehaviour
 {
-    public Transform[] points;
-    
+    public curves[] beziers;
+    public GameObject character;
+
+    public int uMax;
+    public int u = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        uMax = beziers.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //character.transform.position = 
     }
-
 
 }
